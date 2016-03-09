19190d8b53e15b413c81f1c33427d33e775e1d30
Chaomin702 <chaomin702@gmail.com>
2016-03-09 13:18:14 +0800

    LCS print in O(1) space

--- chapter15/LCS.cpp
+++ chapter15/LCS.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 int LCS_length(const vector<char>&A,const vector<char>&B);
+void print_LCS(const vector<vector<int>> &c,int m,int n);
 int main(void){
 	vector<char> A = {'A','B','C','B','D','A','B'};
 	vector<char> B = {'B','D','C','A','B','A'};
@@ -11,7 +12,10 @@ int main(void){
 
 int LCS_length(const vector<char>&A,const vector<char>&B){
 	int m = A.size(),n = B.size();
-	int c[m+1][n+1];
+	vector<vector<int>> c;
+	c.resize(m+1);
+	for(auto &i:c)
+		i.resize(n+1);
 	for(auto &i:c)
 		for (auto &j:i)
 			j = 0;
@@ -24,7 +28,25 @@ int LCS_length(const vector<char>&A,const vector<char>&B){
 			}else{
 				c[i][j] = c[i][j-1];
 			}
+			cout <<"i: "<<i<<" j: "<<j<<" "<< c[i][j] <<endl;
 		}
 	}
+	print_LCS(c,m,n);
 	return c[m][n];
+}
+
+void print_LCS(const vector<vector<int>> &c,int m,int n){
+	if(m==0||n==0){
+		cout << endl;
+		return;
+	}
+	else if(c[m][n]==c[m-1][n]){
+		print_LCS(c,m-1,n);
+	}else if(c[m][n]==c[m][n-1]){
+		print_LCS(c,m,n-1);
+	}else{
+		print_LCS(c,m-1,n-1);
+		cout <<"m: "<<m<<" n: "<<n<<" "<< c[m][n] <<endl;
+	}
+
 }
